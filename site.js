b6385cc1345197a1f546a67aa0d592d34aebd03b
laeti02 <92781000+laeti02@users.noreply.github.com>
2021-12-17 11:20:52 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,8 +1,8 @@
 let logo=document.querySelector('.logo')
 function logochange(){
     
-    logo.setAttribute('src','html/photo/logo.png');
+    logo.setAttribute('src','logo.png');
 }
 function logoback(){
-    logo.setAttribute('src','html/photo/logo.3.png')
+    logo.setAttribute('src','logo.3.png')
 }
