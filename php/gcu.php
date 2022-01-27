6e914e440684958769eca8f0ee33769ff0502cd8
Thibbz <65372633+Thibbz@users.noreply.github.com>
2022-01-27 19:29:12 +0100

    Update gcu.php

--- php/gcu.php
+++ php/gcu.php
@@ -12,14 +12,32 @@
         </div>
         
         <ul>
-            <li onclick="click1()">Article 1</li>
-            <p class="article1"></p>
-            <li onclick="click2()">Article 2</li>
-            <p class="article2"></p>
-            <li onclick="click3()">Article 3</li>
-            <p class="article3"></p>
-            <li onclick="click4()">Article 4</li>
-            <p class="article4"></p>
+            <li onclick="click1()">Article 1 : Object</li>
+            <p class="article1"><br>These T&Cs or General Conditions of Use provide a legal framework for the use of the services of the wekeeptrack site (hereinafter referred to as "the website").<br>
+            Constituting the contract between the company We Preserve, the User, access to the site must be preceded by the acceptance of these T&Cs.<br> Access to this platform signifies the acceptance of these T&Cs.</p>
+            <li onclick="click2()">Article 2 : Legal mentions</li>
+            <p class="article2"><br>The edition of the wekeeptrack site is provided by the company We Preserve, not registered with the RCS.</p>
+            <li onclick="click3()">Article 3 : Website access</li>
+            <p class="article3"><br>The site you are on provides free access to the following services: <br>
+            - Access to heart and GPS data in real time <br>
+            - Access to a little quizz <br>
+            - Access to a personnalisable profile<br>
+            This website is accessible for free for every user with an internet connexion. All the costs to have access to this website are the responsibility of the user. <br>
+            Access to services dedicated to members is done using a username and password.<br>
+            For maintenance or other reasons, access to the site may be interrupted or suspended by the publisher without notice or justification.</p>
+            <li onclick="click4()">Article 4 : collected data</li>
+            <p class="article4"><br>For the creation of the User's account, the collection of information at the time of registration on the site is necessary and mandatory.<br>
+            In accordance with law n ° 78-17 of January 6 relating to data processing, files and freedoms, the collection and processing of personal information is carried out with respect for privacy.<br>
+            According to the Data Protection Act dated January 6, 1978, articles 39 and 40, the User has the right to access, rectify, delete and oppose his personal data. The exercise of this right is carried out by:<br>
+            - The contact form;<br>
+            - His customer area.</p>
+            <li onclick="click5()">Article 5 : Responsibility</li>
+            <p class="article5">Although the information published on the site is deemed reliable, the site reserves the right to not guarantee the reliability of the sources.<br>
+            The information disseminated on the lapetiteperle site is presented for information purposes only and has no contractual value. Despite regular updates, the site cannot be held liable in the event of changes to the administrative and legal provisions appearing after publication. The same applies to the use and interpretation of the information communicated on the platform.<br>
+            The site declines all responsibility for any viruses that may infect the User's computer equipment after using or accessing this site.<br>
+            The site cannot be held responsible in the event of force majeure or the unforeseeable and insurmountable act of a third party.<br>
+            The total guarantee of security and confidentiality of data is not provided by the site. However, the site undertakes to implement all the methods required to do so in the best possible way.
+            </p>
         </ul>
     </body>
 </html>
