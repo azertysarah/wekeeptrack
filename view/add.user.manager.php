93666b941eaffb1e5642e1cb892d1f15e65ef36c
laeti02 <92781000+laeti02@users.noreply.github.com>
2022-01-27 18:15:44 +0100

    Update add.user.manager.php

--- view/add.user.manager.php
+++ view/add.user.manager.php
@@ -61,41 +61,37 @@ require("../modele/connection.php");
     </head>
     <body>
         <div id="bloc_page">
-            <!--header pour toutes les pages-->
-            <div id="top">
-                <img id="fond" src="photo/filigrane.2.png"  >
-                <div id="logo">
-                    <img src="photo/logo.png">
-                    
-                </div>
-                <div id="logotext">
-                    <div id="logo2">
-                        <img  src="photo/facebook.1.png" >
-                        <img  src="photo/insta.1.png">
-                        <img src="photo/linkedin.1.png">
-                        <img src="photo/twitter.1.png">
-                    </div>
-                
-                    <p>Keep track of us</p>
-                </div> 
-            </div>
-            <!--header pour manager-->	
-            <header><!--crée les liens pour aller aux autres pages-->
-                <h2 ><a href="all.the.animals.1.html"> All the animals</a></h2>
-                <h2 ><a href="map.manager.html">Map </a></h2>
-                <h2 ><a href="manage.user.manager.html"> Manage</a></h2>
-                <h2><a href="quizz.html">Quizz</a></h2>
-                <h2><a href="faq.manager.html"> Q&A</a></h2>
-                <h2 class="more" onmouseenter="sizechange()" onmouseleave="sizereturn()">
-                    <a> More</a>
-                    <a id="moretext1" ></a>
-                    <a id="moretext2" ></a>
-                    <a id="moretext3" ></a>
-                </h2>
-                
-            </header>
+		<?php
+            try
+            {
+                // On se connecte à MySQL
+                $mydb = new PDO('mysql:host=localhost;dbname=wekeeptrack;charset=utf8','root', '', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],);
+            }
+            catch(Exception $e)
+            {
+                // En cas d'erreur, on affiche un message et on arrête tout
+                    die('Erreur : '.$e->getMessage());
+            }
+
+            ?>
+    
+    
+            <?php
             
-            <!--fin header-->
+            $username=$_SESSION['username'];
+            $argstatement = $mydb->prepare('SELECT * FROM persons where username=:username');
+            $argstatement->execute(array('username'=>$username));
+            $arguments = $argstatement->fetchAll();
+            if ($arguments[0]['admin']=="yes"){
+                include("header.admin.php");
+            }elseif ($arguments[0]['manager']=="yes"){
+                include("header.manager.php");
+            }else{
+                include("header.user.php");
+            }
+            
+        
+            ?> 
             
         	
         
@@ -151,4 +147,3 @@ require("../modele/connection.php");
 		
     </body>
 </html>
-<script src="Javascript/site.js"></script>
